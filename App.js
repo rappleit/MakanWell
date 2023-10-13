eefea6a196a2783bf7cd43faa51f86f43efce52d
Rachel <80181684+rappleit@users.noreply.github.com>
2023-10-13 22:26:33 +0800

    add bottom tab bar

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Tabs from './navigation/Tabs';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tabs/>
+    </NavigationContainer>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
